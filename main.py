12fd8b876d16c1ca59d13e42c26069b38e98263d
geraldmutsch <102688282+geraldmutsch@users.noreply.github.com>
2022-04-26 17:44:04 +0000

    First String Operation

---
+++ main.py
@@ -0,0 +1,5 @@
+#some examples of string operations
+teacher=input("Name of the Teacher?")   #get the name and story it in a string variable
+welcome="Hello " + teacher + "! You are the teacher today"    #creating the welcome message in a new string
+print (welcome)                         #print the welcome message on the screen
+
