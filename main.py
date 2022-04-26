5ef06c7dba648fb999d283206e099c41fdadfc76
geraldmutsch <102688282+geraldmutsch@users.noreply.github.com>
2022-04-26 17:55:34 +0000

    Second String Operation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #some examples of string operations
-teacher=input("Name of the Teacher?")   #get the name and story it in a string variable
-welcome="Hello " + teacher + "! You are the teacher today"    #creating the welcome message in a new string
+teacher = input("Name of the Teacher?")   #get the name and story it in a string variable
+welcome = "Hello " + teacher + "! You are the teacher today"    #creating the welcome message in a new string
 print (welcome)                         #print the welcome message on the screen
+
+#work with indices
+OneLetter = teacher[4]              #Get the 5th letter of the teachers name
+print ("The 5th letter in the teacher's name is " + OneLetter)
+print ("The name of the teacher is " + str(len(teacher)) + " characters")    #get the length of a string
 
